8653b31c334dd02f1d169fb5416bed2435149845
debop <debop@coupang.com>
2018-06-17 21:02:22 +0900

    [SDR-001] kotlin `@JvmDefault` 관련 설정 제거 * kotlin-stdlib 1.2.50 부터 compiler flag 가 변경되서 문제가 된다. 향후 1.2.51 이상의 안정적 버전이 나오면 다시 추가할 예정임

--- spring-data-requery-java/src/main/java/com/coupang/springframework/data/requery/configs/AbstractRequeryConfiguration.java
+++ spring-data-requery-java/src/main/java/com/coupang/springframework/data/requery/configs/AbstractRequeryConfiguration.java
@@ -11,8 +11,7 @@ import io.requery.sql.ConfigurationBuilder;
 import io.requery.sql.EntityDataStore;
 import io.requery.sql.SchemaModifier;
 import io.requery.sql.TableCreationMode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,10 +28,9 @@ import java.util.Set;
  * @author debop@coupang.com
  * @since 18. 6. 4
  */
+@Slf4j
 @Configuration
 public abstract class AbstractRequeryConfiguration {
-
-    private static final Logger log = LoggerFactory.getLogger(AbstractRequeryConfiguration.class);
 
     @Inject
     ApplicationContext applicationContext;
