ea5d529908a589781fad9566fcc50487d84b4350
debop <debop@coupang.com>
2018-06-22 00:27:05 +0900

    [SDR-000] Interface default method 를 실행하는 예제 추가

--- spring-data-requery-java/src/test/java/com/coupang/springframework/data/requery/repository/query/RequeryParametersTest.java
+++ spring-data-requery-java/src/test/java/com/coupang/springframework/data/requery/repository/query/RequeryParametersTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static com.coupang.springframework.data.requery.repository.query.RequeryParameters.RequeryParameter;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -59,10 +60,42 @@ public class RequeryParametersTest {
         assertThat(parameter.isExplicitlyNamed()).isFalse();
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void runInterfaceDefaultMethod() throws Exception {
+
+        Method method = SampleRepository.class.getMethod("findByName", String.class);
+        assertThat(method).isNotNull();
+        assertThat(method.isDefault()).isTrue();
+
+        // HINT: 이렇게 default method 에 대해서도 직접 실행이 가능하도록 할 수 있다.
+        //
+        SampleRepository repository = new SampleRepositoryImpl();
+        Optional<String> result = (Optional<String>) method.invoke(repository, "value1");
+        assertThat(result.get()).isEqualTo("value1");
+    }
+
     interface SampleRepository {
 
         void foo(@Param("time") LocalDateTime localTime, @Param("firstname") String name);
 
         List<Tuple> bar(int age, String email);
+
+
+        // 이렇게 default method 에 대해서도 직접 실행이 가능하도록 할 수 있다.
+        default Optional<String> findByName(String name) {
+            return Optional.ofNullable(name);
+        }
+    }
+
+    class SampleRepositoryImpl implements SampleRepository {
+        @Override
+        public void foo(LocalDateTime localTime, String name) {
+        }
+
+        @Override
+        public List<Tuple> bar(int age, String email) {
+            return null;
+        }
     }
 }
